9650bb28b8359d789aee2495f5400a8486b5afcd
1180300708 <61471343+1180300708@users.noreply.github.com>
2022-06-02 15:56:32 +0800

    666

--- run.py
+++ run.py
@@ -85,23 +85,21 @@ class SOSearcher(object):
         return return_so
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def mainpage():
-    return render_template("index.html")
+    if request.method == 'GET':
+        return render_template("github issue recommender.html")
+    else:
+        selected_api = request.form.get("selected_api")
+        so_query = request.form.get("so_query")
+        print(selected_api, so_query)
 
+        # 需要构造一个列表，其中的每一项内容需要包括:链接（由ID号进行构造）,标题，内容, Id, AcceptedAnswerId,
+        # processed_body_text,score, viewcount, tag_list, AnswerCount, CommentCount
+        SO_Seacher = SOSearcher(selected_api, so_query)
+        result = SO_Seacher.search()
 
-@app.route("/soresult", methods=["POST"])
-def soresult():
-    selected_api = request.form.get("selected_api")
-    so_query = request.form.get("so_query")
-    print(selected_api, so_query)
-
-    # 需要构造一个列表，其中的每一项内容需要包括:链接（由ID号进行构造）,标题，内容, Id, AcceptedAnswerId,
-    # processed_body_text,score, viewcount, tag_list, AnswerCount, CommentCount
-    SO_Seacher = SOSearcher(selected_api, so_query)
-    result = SO_Seacher.search()
-
-    return render_template("so_results.html", u=result, api=selected_api, query=so_query)
+        return render_template("so_results.html", u=result, api=selected_api, query=so_query)
 
 
 if __name__ == '__main__':
